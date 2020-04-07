e1b804a0bd8f5b4af56c025ae6e09b587cd32fe0
kepet19 <kevinmeejach@gmail.com>
2020-04-07 14:38:57 +0200

    ups put the wrong extension on gitignnore. *.java to *.class instead

---
+++ src/main/java/dk/creditoro/client/MainController.java
@@ -0,0 +1,18 @@
+package dk.creditoro.client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MainController implements Initializable {
+
+
+	public MainController() {
+	}
+
+	@Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+	}
+}
