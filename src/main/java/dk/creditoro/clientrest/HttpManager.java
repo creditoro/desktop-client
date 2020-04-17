bb4af66536b4eb2133d9e84f95e330e78742d791
kepet19 <kevinmeejach@gmail.com>
2020-04-17 15:41:38 +0200

    Refactoret HttpManager to remove duplicate lines, goes from 179lines to 119

--- src/main/java/dk/creditoro/clientrest/HttpManager.java
+++ src/main/java/dk/creditoro/clientrest/HttpManager.java
@@ -1,11 +1,15 @@
 package dk.creditoro.clientrest;
 
 import dk.creditoro.exceptions.HttpStatusException;
+
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -19,161 +23,97 @@ import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 public class HttpManager {
+	private HttpHost host;
 
-    private String hostURL;
-    private int port;
-    private String scheme;
-
-    public HttpManager(String host, int port, String scheme)
+    public HttpManager(String hostname, int port, String scheme)
     {
-        this.hostURL = host;
-        this.port = port;
-        this.scheme = scheme;
+        host = new HttpHost(hostname, port, scheme);
     }
 
     public String get(String path, String query) throws IOException
     {
-        CloseableHttpResponse response = null;
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()){
-            HttpHost host = new HttpHost(hostURL, port, scheme);
+		//Specify get request
+		// TODO need to refactor this line, so there is not "?=" in the query
+		HttpGet request = new HttpGet(path + "?=" + query);
 
-            //Specify get request
-            HttpGet req = new HttpGet(path + "?=" + query);
-
-            //Response
-            response = httpClient.execute(host, req);
-            HttpEntity entity = response.getEntity();
-
-            //JSON to String
-            return streamToString(entity);
-
-        } finally{
-            try {
-                if (response != null) {
-                    response.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+		// execute the Http request and return the string value from the httpEntity
+		return ExecuteHttp(request);
     }
 
     public String get(String path, String query, String token) throws IOException
-    {
-        CloseableHttpResponse response = null;
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpHost host = new HttpHost(hostURL, port, scheme);
+	{
+		//Specify get request
+		// TODO need to refactor this line, so there is not "?=" in the query
+		HttpGet get  = new HttpGet(path + "?=" + query);
 
-            //Specify get request
-            HttpGet p = new HttpGet(path + "?=" + query);
+		//Add Token
+		get.addHeader(new BasicHeader("Authorization", token));
 
-            //Add Token
-            p.addHeader(new BasicHeader("Authorization", token));
+		// execute the Http request and return the string value from the httpEntity
+		return ExecuteHttp(get);
+	}
 
-            //Response
-            response = httpClient.execute(host, p);
-            HttpEntity entity = response.getEntity();
+    public String post(String path, JSONObject json, String token) throws IOException, HttpStatusException 
+	{
+		//Specify post request
+		HttpPost request = new HttpPost(path);
 
-            //JSON to String
-            return streamToString(entity);
+		//Add Token
+		request.addHeader(new BasicHeader("Authorization", token));
+		//String Entity
+		StringEntity postEntity = new StringEntity(json.toString(), ContentType.APPLICATION_JSON);
+		request.setEntity(postEntity);
 
-        } finally{
-            try {
-                if (response != null) {
-                    response.close();
-                }
-            } catch (IOException e) {
-                // use logger here TODO
-            }
-        }
+		// execute the Http request and return the string value from the httpEntity
+		return ExecuteHttp(request);
     }
 
-    public String post(String path, JSONObject json, String token) throws IOException, HttpStatusException {
-        CloseableHttpResponse response = null;
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()){
-            HttpHost host = new HttpHost(hostURL, port, scheme);
+    public String post(String path, JSONObject json) throws IOException, HttpStatusException
+	{
+		//Specify post request
+		HttpPost request = new HttpPost(path);
 
-            //Specify post request
-            HttpPost post = new HttpPost(path);
+		//String Entity
+		StringEntity postEntity = new StringEntity(json.toString(), ContentType.APPLICATION_JSON);
 
-            //Add Token
-            post.addHeader(new BasicHeader("Authorization", token));
+		// Get all information together
+		request.setEntity(postEntity);
 
-            //String Entity
-            StringEntity postEntity = new StringEntity(json.toString(), ContentType.APPLICATION_JSON);
-            post.setEntity(postEntity);
-
-            //Response
-            response = httpclient.execute(host, post);
-            HttpEntity entity = response.getEntity();
-
-            int statusCode = response.getStatusLine().getStatusCode();
-            System.out.println("Status code: " + statusCode);
-
-            if(statusCode != 201)
-            {
-                throw new HttpStatusException("Failed with HTTP error code : " + statusCode);
-            }
-
-            return streamToString(entity);
-
-        } finally{
-            try {
-                if (response != null) {
-                    response.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public String post(String path, JSONObject json) throws IOException, HttpStatusException {
-        CloseableHttpResponse response = null;
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()){
-            HttpHost host = new HttpHost(hostURL, port, scheme);
-
-            //Specify post request
-            HttpPost post = new HttpPost(path);
-
-            //String Entity
-            StringEntity postEntity = new StringEntity(json.toString(), ContentType.APPLICATION_JSON);
-            post.setEntity(postEntity);
-
-            //Response
-            response = httpclient.execute(host, post);
-            HttpEntity entity = response.getEntity();
-
-            int statusCode = response.getStatusLine().getStatusCode();
-            System.out.println("Status code: " + statusCode);
-
-            if(statusCode < 200 || statusCode > 299)
-            {
-                throw new HttpStatusException("Failed with HTTP error code : " + statusCode);
-            }
-
-            return streamToString(entity);
-
-        } finally{
-            try {
-                if (response != null) {
-                    response.close();
-                }
-            } catch (IOException e) {
-                // use logger here TODO
-            }
-        }
+		// execute the Http request and return the string value from the httpEntity
+		return ExecuteHttp(request);
     }
 
 
+	// Executes the request
+	public String ExecuteHttp(HttpUriRequest request)
+	{
+		try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+			// Execute Http
+			try (CloseableHttpResponse response = httpclient.execute(host, request)) {
+				int statusCode = response.getStatusLine().getStatusCode();
+				System.out.println(statusCode);
+				if(statusCode < 200 || statusCode > 299) {
+					throw new HttpStatusException("Failed with HTTP error code : " + statusCode);
+				}
 
-    public String streamToString(HttpEntity response) {
-        try {
-            return new BufferedReader(new InputStreamReader(response.getContent())).lines().collect(Collectors.joining("\n"));
-        } catch (IOException e) {
-            // use logger here TODO
-            return null;
-        }
-    }
+				return streamToString(response.getEntity());
+			} catch (HttpStatusException e) {
+				//TODO add LOGGER
+			}
+			return "";
+		} catch (IOException e) {
+			//TODO add LOGGER
+		}
+		return "";
+	}
 
+    public String streamToString(HttpEntity response)
+	{
+		try {
+			return new BufferedReader(new InputStreamReader(response.getContent())).lines().collect(Collectors.joining("\n"));
+		} catch (IOException e) {
+			// TODO use logger here 
+			return null;
+		}
+	}
 }
