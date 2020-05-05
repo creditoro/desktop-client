1db1b771c37d3ace3f6d8ae64e345351e8772439
kepet19 <kevinmeejach@gmail.com>
2020-05-05 10:05:54 +0200

    Add ClientFactoryTest.java test if we can create clients

--- src/test/java/dk/creditoro/client/core/ClientFactoryTest.java
+++ src/test/java/dk/creditoro/client/core/ClientFactoryTest.java
@@ -1,9 +1,35 @@
 package dk.creditoro.client.core;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+
 /**
 * ClientFactoryTest
 */
 public class ClientFactoryTest {
+	ClientFactory clientFactory;
+	public ClientFactoryTest(){
+		clientFactory = new ClientFactory();
+	}
 
-	
+	@Test
+	public void notNullRestClient(){
+		assertNotNull(clientFactory.getRestClient());	
+	}
+	@Test	
+	public void getRestClient(){
+		assertEquals(clientFactory.getRestClient(), clientFactory.getRestClient());	
+	}
+
+	@Test
+	public void notNullDummyClient(){
+		assertNotNull(clientFactory.getDummyClient());	
+	}
+	@Test	
+	public void getDummyClient(){
+		assertEquals(clientFactory.getDummyClient(), clientFactory.getDummyClient());	
+	}
 }
