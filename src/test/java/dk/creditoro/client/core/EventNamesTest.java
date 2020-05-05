5371baf1d328dfde4bee1643d135a4b80e025dc4
kepet19 <kevinmeejach@gmail.com>
2020-05-05 12:09:36 +0200

    Fixed code smell on EventNamesTest.java

--- src/test/java/dk/creditoro/client/core/EventNamesTest.java
+++ src/test/java/dk/creditoro/client/core/EventNamesTest.java
@@ -12,12 +12,14 @@ public class EventNamesTest {
 	@Test
 	public void checkEnumes(){
 		String message = "Check EventNames enumes";
-		assertEquals(EventNames.LOGIN_RESULT.toString(),
-				"LOGIN_RESULT", message);
-		assertEquals(EventNames.ON_SEARCH_CHANNELS_RESULT.toString(), 
-				"ON_SEARCH_CHANNELS_RESULT", message);
-		assertEquals(EventNames.ON_SEARCH_PRODUCTIONS_RESULT.toString(),
-				"ON_SEARCH_PRODUCTIONS_RESULT", message);
+		assertEquals("LOGIN_RESULT", EventNames.LOGIN_RESULT.toString(), 
+				message);
+		assertEquals("ON_SEARCH_CHANNELS_RESULT", 
+				EventNames.ON_SEARCH_CHANNELS_RESULT.toString(), 
+				message);
+		assertEquals("ON_SEARCH_PRODUCTIONS_RESULT", 
+				EventNames.ON_SEARCH_PRODUCTIONS_RESULT.toString(),
+				message);
 	}
 	
 }
