8170decc856a66dac9d57c0285bd95d8f5087e13
kepet19 <kevinmeejach@gmail.com>
2020-05-05 11:08:38 +0200

    Add EventNamesTest for checking enumes is the same? not sure if it is need?

--- src/test/java/dk/creditoro/client/core/EventNamesTest.java
+++ src/test/java/dk/creditoro/client/core/EventNamesTest.java
@@ -1,9 +1,23 @@
 package dk.creditoro.client.core;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 /**
 * EventNames
 */
 public class EventNamesTest {
 
+	@Test
+	public void checkEnumes(){
+		String message = "Check EventNames enumes";
+		assertEquals(EventNames.LOGIN_RESULT.toString(),
+				"LOGIN_RESULT", message);
+		assertEquals(EventNames.ON_SEARCH_CHANNELS_RESULT.toString(), 
+				"ON_SEARCH_CHANNELS_RESULT", message);
+		assertEquals(EventNames.ON_SEARCH_PRODUCTIONS_RESULT.toString(),
+				"ON_SEARCH_PRODUCTIONS_RESULT", message);
+	}
 	
 }
