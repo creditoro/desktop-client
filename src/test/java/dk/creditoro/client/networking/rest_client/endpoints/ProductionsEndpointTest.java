b5748affac28571f2849af24a3d41d082963b599
kepet19 <kevinmeejach@gmail.com>
2020-05-06 11:41:50 +0200

    Add a new method too ProductionsEndpoint that can take a token then it post. Add productionsEndpoint unit test

--- src/test/java/dk/creditoro/client/networking/rest_client/endpoints/ProductionsEndpointTest.java
+++ src/test/java/dk/creditoro/client/networking/rest_client/endpoints/ProductionsEndpointTest.java
@@ -1,9 +1,65 @@
 package dk.creditoro.client.networking.rest_client.endpoints;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import dk.creditoro.client.model.crud.Production;
+import dk.creditoro.client.networking.IClient;
+import dk.creditoro.client.networking.rest_client.RestClient;
+import kong.unirest.json.JSONObject;
+
 /**
 * ProductionsEndpointTest
 */
 public class ProductionsEndpointTest {
-
+	ProductionsEndpoint productionsEndpoint;
+	IClient client;
+	String token;
+	Production[] productions;
 	
+	public ProductionsEndpointTest(){
+		productionsEndpoint = new ProductionsEndpoint(new HttpManager());
+		client = new RestClient();
+		token = client.login("string@string.dk", "string");
+	}
+
+	@BeforeEach @Test
+	void getProductions(){
+		productions = productionsEndpoint.getProductions("", token);
+		assertNotNull(productions);
+	}
+
+	// TODO add this test back then productions works @Test
+	void getProduction(){
+		assertEquals(productions[0].getIdentifier(), 
+				productionsEndpoint.getProduction(productions[0]
+					.getIdentifier()).getIdentifier(),
+				"Check if we can get the same productions");
+	}
+
+	@Test
+	void putProduction(){
+		assertNull(productionsEndpoint.putProduction("", null), 
+				"Fix this then the putProduction works");
+	}
+
+	@Test
+	void patchProduction(){
+		assertNull(productionsEndpoint.patchProduction("", null), 
+				"Fix this then the patchProduction works");
+	}
+
+	@Test
+	void postProduction(){
+        var body = new JSONObject(Map.of("title", "Ringes Herrer", "producer_id", "pro10-10", "channel_id", "chan10-10"));
+		assertTrue(productionsEndpoint.postProduction(body) == null, 
+				"Fix this then productions works in the API");
+	}
 }
