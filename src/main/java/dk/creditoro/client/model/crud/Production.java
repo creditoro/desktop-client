c4f044c0b4db48d47977ef73d2d8872ed93ddfe1
mara816 <mara816@student.sdu.dk>
2020-05-18 11:47:40 +0200

    Removed category from Production

--- src/main/java/dk/creditoro/client/model/crud/Production.java
+++ src/main/java/dk/creditoro/client/model/crud/Production.java
@@ -6,16 +6,14 @@ public class Production {
     private final String identifier;
     private final String title;
     private final String description;
-    private final String[] category;
     private final User producer;
     private final Channel channel;
 
 
-    public Production(String identifier, String title, String description, String[] category, User producer, Channel channel) {
+    public Production(String identifier, String title, String description, User producer, Channel channel) {
         this.identifier = identifier;
         this.title = title;
         this.description = description;
-        this.category = category;
         this.producer = producer;
         this.channel = channel;
 
@@ -31,10 +29,6 @@ public class Production {
 
     public String getDescription() {
         return description;
-    }
-
-    public String[] getCategory() {
-        return category;
     }
 
     public User getProducer() {
