8339387ba9cadb6f8185a74397dacc312f536351
kepet19 <kevinmeejach@gmail.com>
2020-05-18 15:40:26 +0200

    Deleted a unsued test, and add a new unittest

---
+++ src/test/java/dk/creditoro/client/view/production/ProductionViewModelTest.java
@@ -0,0 +1,14 @@
+package dk.creditoro.client.view.production;
+
+import org.junit.jupiter.api.Test;
+
+/**
+* ProductionViewModelTest
+*/
+public class ProductionViewModelTest {
+
+	
+	@Test
+	void getProductions(){
+	}
+}
