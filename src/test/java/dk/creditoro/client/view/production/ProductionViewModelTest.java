285583f7dc489befca448b51493b522ebe388abc
kepet19 <kevinmeejach@gmail.com>
2020-05-19 10:40:52 +0200

    I am not sure how to write test to this yet.

--- src/test/java/dk/creditoro/client/view/production/ProductionViewModelTest.java
+++ src/test/java/dk/creditoro/client/view/production/ProductionViewModelTest.java
@@ -1,10 +1,15 @@
 package dk.creditoro.client.view.production;
 
+import java.util.concurrent.CountDownLatch;
+
+import javax.swing.SwingUtilities;
+
 import org.junit.jupiter.api.*;
 
 import dk.creditoro.client.core.ClientFactory;
 import dk.creditoro.client.core.ModelFactory;
 import dk.creditoro.client.core.ViewModelFactory;
+import javafx.embed.swing.JFXPanel;
 
 /**
 * ProductionViewModelTest
@@ -13,6 +18,19 @@ public class ProductionViewModelTest {
 	ProductionViewModel pdViewModel;
 
 	public ProductionViewModelTest() {
+		final CountDownLatch latch = new CountDownLatch(1);
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new JFXPanel(); // initializes JavaFX environment
+				latch.countDown();
+			}
+		});
+		try{
+			latch.await();
+		} catch(InterruptedException e){
+		}
+
+		//Makes the client that is used to connect to the internet
 		ModelFactory mlF = new ModelFactory(new ClientFactory());
 		ViewModelFactory vmlF = new ViewModelFactory(mlF);
 		pdViewModel = vmlF.getProductionViewModel();
@@ -21,6 +39,7 @@ public class ProductionViewModelTest {
 	@Test
 	void changeQueryParam(){
 		pdViewModel.queryParamProperty().setValue("This");
-		pdViewModel.getCredits();
+		pdViewModel.setId("This should not be set like this??? ");
+		// pdViewModel.getCredits();
 	}
 }
