65adba5cfcb56c5acd464b5468a52aebf1613116
kepet19 <kevinmeejach@gmail.com>
2020-05-19 10:22:09 +0200

    Removed unused imports

--- src/test/java/dk/creditoro/client/view/production/ProductionViewModelTest.java
+++ src/test/java/dk/creditoro/client/view/production/ProductionViewModelTest.java
@@ -1,14 +1,26 @@
 package dk.creditoro.client.view.production;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import dk.creditoro.client.core.ClientFactory;
+import dk.creditoro.client.core.ModelFactory;
+import dk.creditoro.client.core.ViewModelFactory;
 
 /**
 * ProductionViewModelTest
 */
 public class ProductionViewModelTest {
+	ProductionViewModel pdViewModel;
 
-	
+	public ProductionViewModelTest() {
+		ModelFactory mlF = new ModelFactory(new ClientFactory());
+		ViewModelFactory vmlF = new ViewModelFactory(mlF);
+		pdViewModel = vmlF.getProductionViewModel();
+	}
+
 	@Test
-	void getProductions(){
+	void changeQueryParam(){
+		pdViewModel.queryParamProperty().setValue("This");
+		pdViewModel.getCredits();
 	}
 }
