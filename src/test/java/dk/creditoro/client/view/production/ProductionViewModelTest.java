e9e2dbdcac8436ed7f3ea0db6d55b4f906d742b9
kepet19 <kevinmeejach@gmail.com>
2020-05-20 16:03:32 +0200

    Add more unittest to ProductionViewModelTest

--- src/test/java/dk/creditoro/client/view/production/ProductionViewModelTest.java
+++ src/test/java/dk/creditoro/client/view/production/ProductionViewModelTest.java
@@ -11,12 +11,15 @@ import org.junit.jupiter.api.*;
 import dk.creditoro.client.core.ClientFactory;
 import dk.creditoro.client.core.*;
 import javafx.embed.swing.JFXPanel;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * ProductionViewModelTest
  */
 class ProductionViewModelTest {
 	ProductionViewModel pdViewModel;
+	ViewModelFactory vmlF;
 
 	public ProductionViewModelTest() {
 		final CountDownLatch latch = new CountDownLatch(1);
@@ -33,7 +36,7 @@ class ProductionViewModelTest {
 
 		//Makes the client that is used to connect to the internet
 		ModelFactory mlF = new ModelFactory(new ClientFactory());
-		ViewModelFactory vmlF = new ViewModelFactory(mlF);
+		vmlF = new ViewModelFactory(mlF);
 		pdViewModel = vmlF.getProductionViewModel();
 	}
 
@@ -63,5 +66,45 @@ class ProductionViewModelTest {
 		creditsList = pdViewModel.listPropertyProperty();
 		assertEquals(job, creditsList.get(0).getJob(),
 				"Maybe credits changed?");
+	}
+
+	@Test
+	void getAndSetIds(){
+		//Get set ID
+		assertDoesNotThrow(()-> pdViewModel.setId("thisId"));
+		assertEquals("thisId", pdViewModel.getId());
+	
+		//Get set channelId
+		assertDoesNotThrow(()-> pdViewModel.setChannelId("channelId"));
+		assertEquals("channelId", pdViewModel.getChannelId());
+		
+	}
+
+	@Test
+	void getAndSetLogo(){
+		var channelViewModel = vmlF.getBrowseChannelsViewModel();
+		// Searching for at channel to gets it id
+		channelViewModel.queryParamProperty().setValue("TV");
+		channelViewModel.search();
+		// Extract the channels and id from it 
+		var channelIdList = channelViewModel.listPropertyProperty();
+		var channelId = channelIdList.get(0).getIdentifier();
+
+		// Sets the new Channel ID
+		pdViewModel.setChannelId(channelId);
+		//Get set ChannelLogo
+		//
+		// Refresesh the logo
+		assertDoesNotThrow(() -> pdViewModel.refreshLogo());
+
+		var imageView = new ImageView();
+		assertNotEquals(imageView, pdViewModel.getChannelLogo(), 
+				"This logo should never be the same, if it ise then change the channelId");
+
+		// set the image view
+		assertDoesNotThrow(()-> pdViewModel.setChannelLogo(imageView));
+		assertEquals(imageView, pdViewModel.getChannelLogo());	
+		
+
 	}
 }
