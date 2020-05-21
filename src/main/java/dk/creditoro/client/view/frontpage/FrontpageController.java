238980a8b31b52b6072ea79dd8f6837287e61e52
Kechr <kechr19@student.sdu.dk>
2020-05-21 14:43:08 +0200

    yes

--- src/main/java/dk/creditoro/client/view/frontpage/FrontpageController.java
+++ src/main/java/dk/creditoro/client/view/frontpage/FrontpageController.java
@@ -22,14 +22,10 @@ import java.util.logging.Logger;
  */
 public class FrontpageController implements IViewController {
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    ObservableList<String> searchingList = FXCollections.observableArrayList("Produktion", "Kanal");
     private ViewHandler viewHandler;
-
     @FXML
     private ChoiceBox<String> choiceBox;
-
-    ObservableList<String> searchingList = FXCollections.observableArrayList("Produktion", "Kanal");
-
-
     private TranslateTransition openNav;
     private TranslateTransition openBtn;
     private TranslateTransition closeNav;
