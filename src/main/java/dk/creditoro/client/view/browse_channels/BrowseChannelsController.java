124ca80d50dd3bbfaaff1394d9c9c2f8c84b537d
Simon Jørgensen <55147377+Joergenson@users.noreply.github.com>
2020-05-22 12:52:13 +0200

    fixed infinity loop
    but productions are showing up the wrong places

--- src/main/java/dk/creditoro/client/view/browse_channels/BrowseChannelsController.java
+++ src/main/java/dk/creditoro/client/view/browse_channels/BrowseChannelsController.java
@@ -102,7 +102,7 @@ public class BrowseChannelsController implements IViewController {
         viewHandler.openView(Views.CHANNEL_PROGRAMS);
         viewModelFactory.getChannelProgramsViewModel().setId(viewToOpen);
         viewModelFactory.getChannelProgramsViewModel().queryParamProperty().setValue("");
-        viewModelFactory.getChannelProgramsViewModel().search();
+        viewModelFactory.getChannelProgramsViewModel().getProductions();
 
     }
 
